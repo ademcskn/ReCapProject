f8cd6f7b0aff81ae5ee276a35e5db18687d54b7c
ademcskn <ademcskn@hotmail.com>
2022-03-03 18:11:33 +0300

    Güncellendi

---
+++ Core/CrossCuttingConcerns/Microsoft/MemoryCacheManager.cs
@@ -0,0 +1,39 @@
+﻿using Core.CrossCuttingConcerns.Caching;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Microsoft
+{
+    public class MemoryCacheManager : ICacheManager
+    {
+        IMemoryCache _memoryCache;
+        public void Add(string key, object value, int duration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAdd(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
